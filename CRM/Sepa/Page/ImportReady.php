852e05084c433eea028febec8e23e241dffc74d9
scardinius <scardinius@chords.pl>
2016-06-23 13:48:09 +0200

    Add dividing import file into batches #396

--- CRM/Sepa/Page/ImportReady.php
+++ CRM/Sepa/Page/ImportReady.php
@@ -4,18 +4,23 @@ require_once 'CRM/Core/Page.php';
 
 class CRM_Sepa_Page_ImportReady extends CRM_Core_Page {
 
+  private $batchSize = 3;
+
   function run() {
     $session = new CRM_Core_Session();
     $data = $session->get('data', 'sepa-import');
-    $params = $session->get('params', 'sepa-import');
-    $this->assign('count', count($data));
-    $this->assign('params', $params);
+    $this->assign('rows', count($data) - 1);
 
     $queue = CRM_Sepa_Logic_ImportQueue::singleton()->getQueue();
+    if (!$queue->numberOfItems()) {
+      $this->addTaskStarting($queue);
 
-    // todo divide data into batches per 50 rows
-    $this->addTaskStarting($queue);
-    $this->addTaskCreateMandate($queue, $data);
+      $batches = ceil((count($data) - 1) / $this->batchSize);
+      for ($i = 1; $i <= $batches; $i++) {
+        $batch = array_slice($data, $i * $this->batchSize - $this->batchSize, $this->batchSize);
+        $this->addTaskCreateMandate($queue, $batch, $i, $batches);
+      }
+    }
 
     return parent::run();
   }
@@ -27,7 +32,8 @@ class CRM_Sepa_Page_ImportReady extends CRM_Core_Page {
   private function addTaskStarting(CRM_Queue_Queue &$queue) {
     $task = new CRM_Queue_Task(
       array('CRM_Sepa_Logic_ImportTasks', 'starting'),
-      array()
+      array(),
+      'Starting import mandates'
     );
     $queue->createItem($task);
   }
@@ -35,12 +41,15 @@ class CRM_Sepa_Page_ImportReady extends CRM_Core_Page {
 
   /**
    * @param \CRM_Queue_Queue $queue
-   * @param $data
+   * @param array $batch
+   * @param int $counter
+   * @param int $n
    */
-  private function addTaskCreateMandate(CRM_Queue_Queue &$queue, $data) {
+  private function addTaskCreateMandate(CRM_Queue_Queue &$queue, $batch, $counter, $n) {
     $task = new CRM_Queue_Task(
       array('CRM_Sepa_Logic_ImportTasks', 'createMandates'),
-      array($data)
+      array($batch),
+      'Created mandates in batch '.$counter."/".$n
     );
     $queue->createItem($task);
   }
