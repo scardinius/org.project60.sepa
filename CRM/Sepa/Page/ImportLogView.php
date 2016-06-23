2a950ffc5947edd9004c43958635729bb71aa5f6
scardinius <scardinius@chords.pl>
2016-06-23 13:45:53 +0200

    Clear session variables at the end of import #396

--- CRM/Sepa/Page/ImportLogView.php
+++ CRM/Sepa/Page/ImportLogView.php
@@ -5,6 +5,9 @@ require_once 'CRM/Core/Page.php';
 class CRM_Sepa_Page_ImportLogView extends CRM_Core_Page {
 
   function run() {
+    $session = new CRM_Core_Session();
+    $session->set('data', null, 'sepa-import');
+    $session->set('params', null, 'sepa-import');
     return parent::run();
   }
 }
